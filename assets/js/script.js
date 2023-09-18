964f6d85e01f279afa2a6bef88265ed0659af080
amy-1989 <>
2023-09-18 10:53:04 +0000

    add favicon and final tidyups for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,14 +14,25 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
 
-        runGame("addition");
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    });
+    runGame("addition");
+
 });
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     //creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -33,7 +44,7 @@ function runGame(gameType) {
     }
     else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    } 
+    }
     else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -74,8 +85,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     }
     else if (operator === "-") {
-        return [operand1 - operand2, "subtract"];}
-        else {
+        return [operand1 - operand2, "subtract"];
+    }
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -108,8 +120,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractQuestion(operand1, operand2) {
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1: operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2: operand1;
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
 
 }
